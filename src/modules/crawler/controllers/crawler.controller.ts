1e0b306a11d9d1486c0b1fbfa25c9413b10835ea
guansunyata <guansunyata@gmail.com>
2019-12-12 23:22:49 +0800

    Init puppeteer with a sample

--- src/modules/crawler/controllers/crawler.controller.ts
+++ src/modules/crawler/controllers/crawler.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Param } from '@nestjs/common'
 import { CrawlerService } from '../services/crawler.service'
 
 @Controller('crawler')
 export class CrawlerController {
   constructor(private readonly crawlerService: CrawlerService) {}
 
-  @Get()
-  find(): string {
-    this.crawlerService.create('Text')
-    return JSON.stringify(this.crawlerService.findAll())
+  @Get(':keyword')
+  async find(@Param() params): Promise<string> {
+    const keyword = decodeURIComponent(params.keyword)
+    const data = await this.crawlerService.fetchPage(keyword)
+    return JSON.parse(data)
   }
 }
--- src/modules/crawler/services/crawler.service.ts
+++ src/modules/crawler/services/crawler.service.ts
@@ -1,16 +1,70 @@
+import _ from 'lodash'
+import puppeteer from 'puppeteer'
 import { Injectable } from '@nestjs/common'
+import { SearchResult } from '../types/search-result.types'
 
 @Injectable()
 export class CrawlerService {
-  private readonly arr: Result[] = []
+  private readonly arr: ResultStore = {}
 
-  create(result: Result) {
-    this.arr.push(result)
+  store(keyword: string, result: ResultStore) {
+    this.arr[keyword] = result
   }
 
-  findAll(): Result[] {
+  findAll(): ResultStore[] {
     return this.arr
+  }
+
+  async fetchPage(keyword: string): Promise<string> {
+    // go
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto(getUrl(keyword), gotoOptions)
+
+    // evaluates
+    await page.evaluate(removeContent)
+    await page.waitForSelector('h3')
+    const data = await page.evaluate(getRankData)
+
+    browser.close()
+
+    // cache
+    this.store(keyword, data)
+
+    return Promise.resolve(data)
   }
 }
 
-type Result = any
+const getUrl = (query, page = 1) => {
+  const q = encodeURIComponent(query)
+  const start = (page === 1) ? 0 : page++ * 10
+  return `https://www.google.com/search?q=${q}&start=${start}`
+}
+
+const gotoOptions = {
+  waitUntil: 'networkidle0' as const,
+  timeout: 6000,
+}
+
+type ResultStore = any
+
+// in browser
+const getRankData = () => {
+  const elements = document.querySelectorAll('h3 > span')
+  const data = [].slice.call(elements).map((element: HTMLElement, index) => {
+    const link = element.parentElement.parentElement.getAttribute('href')
+    return {
+      rank: index + 1,
+      link,
+      title: element.innerText,
+    } as SearchResult
+  })
+  return JSON.stringify(data)
+}
+
+const removeContent = () => {
+  const elements = document.querySelectorAll('script, link, style')
+  for (const e of Array.from(elements)) {
+    e.remove()
+  }
+}
